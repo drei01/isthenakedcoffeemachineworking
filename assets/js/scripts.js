c1d07a5da753a780b5c5468483940dc8ad562359
drei01 <mcreidie@hotmail.com>
2013-07-24 09:37:11 +0100

    initial import

---
+++ assets/js/scripts.js
@@ -0,0 +1,36 @@
+
+jQuery(document).ready(function() {
+
+    /*
+        Background slideshow
+    */
+    $('.coming-soon').backstretch([
+      "assets/img/backgrounds/1.jpg"
+    , "assets/img/backgrounds/2.jpg"
+    , "assets/img/backgrounds/3.jpg"
+    ], {duration: 3000, fade: 750});
+
+    /*
+        Countdown initializer
+    */
+    var now = new Date();
+    var countFrom = '2013/07/23';
+    $('.timer').countdown(countFrom, function(event) {
+        var $this = $(this);
+        switch(event.type) {
+            case "seconds":
+            case "minutes":
+            case "hours":
+            case "days":
+            case "weeks":
+            case "daysLeft":
+                $this.find('span.'+event.type).html(event.value);
+                break;
+            case "finished":
+                $this.hide();
+                break;
+        }
+    });
+
+});
+
